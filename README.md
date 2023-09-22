e6d38a1f5c9beb29ed1012c2bd81e1587e5cdeb1
Caroline Trombeta <carolinecampost@gmail.com>
2023-09-22 07:56:15 +0100

    Final version. Documentation updated.

--- README.md
+++ README.md
@@ -1,12 +1,91 @@
-# Documentação do Projeto Cordova
+# Documentação do Projeto MA Silva
 
 ## Introdução
 
-Esta é a documentação do projeto Cordova para facilitar o desenvolvimento e a manutenção do seu aplicativo.
+Esse projeto é uma aplicação desenvolvida para a empresa MA Silva, na qual é uma empresa de renome voltada à confecção de rolhas de cortiça e possui foco em sustentabilidade. A aplicação será utilizada em uma feira de stands num dispositivo chamado MUPI que leva o Android (versão 7.1) como sistema operacional.
 
-## Instalação do Cordova
+## Requisitos de Instalação
 
-Certifique-se de ter o Node.js e o npm instalados. Em seguida, instale o Cordova globalmente com o seguinte comando:
+1. Node.js e npm
+2. Java JDK
+3. Android Studio
+4. Cordova
 
-```bash
-npm install -g cordova
+## Uso
+
+1. Clone o repositório
+2. Instale as dependências com `npm install`
+3. Adicione a plataforma Android com `cordova platform add android`
+4. Execute o comando `cordova run android` para instalar a aplicação no dispositivo
+
+## Estrutura do Projeto
+
+O projeto é dividido em 3 pastas principais:
+
+1. `www`: Contém os arquivos HTML, CSS e JavaScript da aplicação
+2. `platforms`: Contém os arquivos nativos da plataforma Android
+3. `plugins`: Contém os plugins utilizados pela aplicação
+4. `res`: Contém os recursos utilizados pela aplicação como ícones e splash screens
+
+## Telas
+
+A aplicação possui 3 terlas principais:
+
+1. Tela de vídeo corporativo
+2. Tela de apresentação de produtos
+3. Tela de informações sobre a visão de sustentabilidade da empresa
+
+## Comandos Úteis
+
+1. Certificar de ter o Node.Js e o npm instalados. Em seguida, instalar o Cordova com o comando:
+`npm install -g cordova`
+
+2. Criar um projeto com o comando:
+ `cordova create <nome do projeto> <nome do pacote> <nome do projeto>`
+
+3. Adicionar plataformas com o comando:
+`cordova platform add <nome da plataforma>`
+Exemplo: `cordova platform add android`
+OBS: para adicionar uma versão específica de Android, utilize o comando:
+`cordova platform add android@<versão>`
+
+4. Adicionar plugins com o comando:
+`cordova plugin add <nome do plugin>`
+Exemplo: `cordova plugin add cordova-plugin-whitelist`
+
+5. Para construir o projeto, utilize o comando:
+`cordova build <nome da plataforma>`
+Exemplo: `cordova build android`
+
+6. Para executar o projeto no dispositivo, utilize o comando:
+`cordova run <nome da plataforma>`
+Exemplo: `cordova run android`
+OBS: em caso de um dispositivo específico conectado via USB, utilize o comando:
+`cordova run android --target=<id do dispositivo>`
+
+7. Para executar o projeto no navegador, utilize o comando:
+`cordova run browser`
+
+8. Para executar o projeto no emulador, utilize o comando:
+`cordova emulate <nome da plataforma>`
+
+9. Para remover uma plataforma, utilize o comando:
+`cordova platform remove <nome da plataforma>`
+Exemplo: `cordova platform remove android`
+
+10. Para remover um plugin, utilize o comando:
+`cordova plugin remove <nome do plugin>`
+Exemplo: `cordova plugin remove cordova-plugin-whitelist`
+
+11. Para listar as plataformas, utilize o comando:
+`cordova platform ls`
+
+12. Para listar os plugins, utilize o comando:
+`cordova plugin ls`
+
+13. Para listar os dispositivos conectados, utilize o comando:
+`adb devices`
+
+## Contato
+
+Para mais informações, entre em contato com o desenvolvedor através do e-mail: `carolinecampost@gmail.com`
--- www/js/index.js
+++ www/js/index.js
@@ -14,21 +14,21 @@ const languages = {
     fr: {
         mainPageButtonVideo: "Vidéo",
         mainPageButtonDurability: "Durabilité",
-        mainPageButtonFamily: "Vivo Famille",
+        mainPageButtonFamily: "Bouchons Viva",
 
         titlePageVideo: "Vidéo",
         titlePageDurability: "Durabilité",
-        titlePageFamily: "Vivo Famille",
+        titlePageFamily: "Bouchons Viva",
 
     },
     en: {
         mainPageButtonVideo: "Video",
         mainPageButtonDurability: "Durability",
-        mainPageButtonFamily: "Vivo Family",
+        mainPageButtonFamily: "Vivo Family", // depois modificar para o inglês
 
         titlePageVideo: "Video",
         titlePageDurability: "Durability",
-        titlePageFamily: "Vivo Family"
+        titlePageFamily: "Vivo Family" // depois modificar para o inglês
     }
 }
 
@@ -134,12 +134,138 @@ function changeLanguage(language) {
 
 })(jQuery);
 
+// function to pause and reset video
 function pauseAndResetVideo() {
     var video = document.getElementById("video_institutional");
     video.pause();
     video.currentTime = 0;
 }
 
+// Função para rolar suavemente de volta ao topo ao sair da página
+function scrollToTop(containerId) {
+    var container = document.getElementById(containerId);
+
+    if (container) {
+        container.scrollTop = 0;
+    }
+}
+
+// Função para rolar suavemente ao clicar no botão "voltar ao topo"
+function scrollToTopSmooth(containerId) {
+    var container = document.getElementById(containerId);
+    var initialScrollTop = container.scrollTop;
+    var startTime = null;
+    var duration = 300; // Duração da animação em milissegundos
+
+    function animateScroll(currentTime) {
+        if (startTime === null) {
+            startTime = currentTime;
+        }
+
+        var elapsedTime = currentTime - startTime;
+        var easing = easeInOutQuad(elapsedTime, 0, 1, duration); // Easing function (veja abaixo)
+
+        container.scrollTop = initialScrollTop - initialScrollTop * easing;
+
+        if (elapsedTime < duration) {
+            requestAnimationFrame(animateScroll);
+        }
+    }
+
+    requestAnimationFrame(animateScroll);
+}
+
+
+// Função de interpolação (easing function) para scroll suave
+function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+}
+
+
+// Função para criar um botão "Voltar ao Topo" e configurar o comportamento suave de rolagem
+function createScrollToTopButton(item) {
+    const containerId = item.id;
+    const container = document.getElementById(containerId);
+
+    // Verifique se o contêiner existe
+    if (!container) {
+        return;
+    }
+
+    // Crie um novo elemento de botão
+    const button = document.createElement('div');
+    button.classList.add('scroll-to-top');
+    button.innerHTML = '<img src="./img/icons8-collapse-arrow-100.png" alt="" class="scroll-to-top-button">';
+    
+    // Adicione o botão ao contêiner
+    container.appendChild(button);
+
+    // Adicione um ouvinte de evento de clique ao botão para rolagem suave
+    button.addEventListener('click', function () {
+        scrollToTopSmooth(containerId);
+    });
+
+    // Adicione um ouvinte de evento de rolagem ao contêiner para mostrar/ocultar o botão
+    container.addEventListener('scroll', function () {
+        if (container.scrollTop > 100) {
+            $(button).fadeIn(1000);
+            button.style.display = 'flex';
+        } else {
+            $(button).fadeOut("slow", function() {
+                button.style.display = 'none';
+            });
+        }
+    });
+}
+
+// aplicando a função para o contêiner de rolagem da seção de durabilidade
+createScrollToTopButton(document.getElementById('scroll_container_durability'));
+
+
+// Selecione todos os itens da lista de navegação com a classe 'scrollable-item'
+const scrollableItems = document.querySelectorAll('.scrollable-item');
+
+// Crie um botão "Voltar ao Topo" para cada item da lista de navegação
+scrollableItems.forEach(function (item) {
+    createScrollToTopButton(item);
+});
+
+var inactivityTimer; // Variável para armazenar o temporizador de inatividade
+var inactivityTimeout = 600000; // Tempo limite de inatividade em milissegundos (5 minutos)
+
+// Função para iniciar o temporizador de inatividade
+function startInactivityTimer() {
+    // Limpa o temporizador se já estiver em execução
+    clearTimeout(inactivityTimer);
+    // Define um novo temporizador de inatividade
+    inactivityTimer = setTimeout(function() {
+        // Redirecione para a página inicial após o tempo limite de inatividade
+        redirectToMainPage();
+    }, inactivityTimeout);
+}
+
+// Evento de clique no body (ou em outro elemento comum a todas as páginas)
+document.body.addEventListener("click", function() {
+    // Reinicie o temporizador de inatividade quando houver interação do usuário
+    startInactivityTimer();
+});
+
+startInactivityTimer();
+
+// Função para redirecionar para a página inicial
+function redirectToMainPage() {
+    actualPage = "main_page";
+    // Adicione aqui o código para ocultar as páginas atuais e mostrar a página inicial
+    $("#carousel_catalog").css("display", "none");
+    $("#institutional_video").css("display", "none");
+    $("#information_durability").css("display", "none");
+    $("#catalog_family").css("display", "none");
+    $("#main_page").fadeIn(1000);
+    $("#main_page").css("display", "block");
+}
 
 // Display default para quando abre a app
 var actualPage = "main_page";
@@ -151,8 +277,15 @@ $("#main_page").css("display", "block");
 
 // Função de clique para navegação entre as telas
 document.addEventListener("click", function(e) {
+    const timeAnimation = 800;
     if (e.target.id === "back_main_page") {
         if (actualPage === "carousel_catalog") {
+            scrollToTop("scroll_container_navigation_1");
+            scrollToTop("scroll_container_navigation_2");
+            scrollToTop("scroll_container_navigation_3");
+            scrollToTop("scroll_container_navigation_4");
+            scrollToTop("scroll_container_navigation_5");
+
             $("#carousel_catalog").css("display", "none");
             $("#catalog_family").fadeIn(1000);
             $("#catalog_family").css("display", "flex");
@@ -161,6 +294,12 @@ document.addEventListener("click", function(e) {
             if (actualPage === "institutional_video") {
                 pauseAndResetVideo();
             }
+
+            if (actualPage === "information_durability") {
+                scrollToTop("scroll_container_durability");
+                console.log("scroll to top");
+            }
+
             $("#carousel_catalog").css("display", "none");
             $("#institutional_video").css("display", "none");
             $("#information_durability").css("display", "none");
@@ -173,35 +312,50 @@ document.addEventListener("click", function(e) {
     }
 
     if (e.target.id === "btn_video"  || e.target.id === "btn_text_video") {
-        actualPage = "institutional_video";
-        $("#main_page").css("display", "none");
-        $("#carousel_catalog").css("display", "none");
-        $("#information_durability").css("display", "none");
-        $("#catalog_family").css("display", "none");
-        $("#institutional_video").fadeIn(1000);
-        $("#institutional_video").css("display", "flex");
-        var video = document.getElementById("video_institutional");
-        video.play();
+        const btnVideo = document.getElementById("btn_video");
+        btnVideo.classList.add("animate__animated", "animate__pulse");
+        setTimeout(function () {
+            btnVideo.classList.remove("animate__animated", "animate__pulse");
+            actualPage = "institutional_video";
+            $("#main_page").css("display", "none");
+            $("#carousel_catalog").css("display", "none");
+            $("#information_durability").css("display", "none");
+            $("#catalog_family").css("display", "none");
+            $("#institutional_video").fadeIn(1000);
+            $("#institutional_video").css("display", "flex");
+            var video = document.getElementById("video_institutional");
+            video.play();
+        }, timeAnimation);
     }
 
     if (e.target.id === "btn_durability" || e.target.id === "btn_text_durability") {
-        actualPage = "information_durability";
-        $("#main_page").css("display", "none");
-        $("#carousel_catalog").css("display", "none");
-        $("#institutional_video").css("display", "none");
-        $("#catalog_family").css("display", "none");
-        $("#information_durability").fadeIn(1000);
-        $("#information_durability").css("display", "flex");
+        const btnDurability = document.getElementById("btn_durability");
+        btnDurability.classList.add("animate__animated", "animate__pulse");
+        setTimeout(function () {
+            btnDurability.classList.remove("animate__animated", "animate__pulse");
+            actualPage = "information_durability";
+            $("#main_page").css("display", "none");
+            $("#carousel_catalog").css("display", "none");
+            $("#institutional_video").css("display", "none");
+            $("#catalog_family").css("display", "none");
+            $("#information_durability").fadeIn(1000);
+            $("#information_durability").css("display", "flex");
+        }, timeAnimation);
     }
 
     if (e.target.id === "btn_family" || e.target.id === "btn_text_family") {
-        actualPage = "catalog_family";
-        $("#main_page").css("display", "none");
-        $("#institutional_video").css("display", "none");
-        $("#information_durability").css("display", "none");
-        $("#carousel_catalog").css("display", "none");
-        $("#catalog_family").fadeIn(1000);
-        $("#catalog_family").css("display", "flex");
+        const btnFamily = document.getElementById("btn_family");
+        btnFamily.classList.add("animate__animated", "animate__pulse");
+        setTimeout(function () {
+            btnFamily.classList.remove("animate__animated", "animate__pulse");
+            actualPage = "catalog_family";
+            $("#main_page").css("display", "none");
+            $("#institutional_video").css("display", "none");
+            $("#information_durability").css("display", "none");
+            $("#carousel_catalog").css("display", "none");
+            $("#catalog_family").fadeIn(1000);
+            $("#catalog_family").css("display", "flex");
+        }, timeAnimation);
     }
 
     console.log(actualPage);
